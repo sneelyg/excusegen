26a684a9ee6d2bf2a3655832db751a48aec1b31d
sneelyg <santiagoneely@gmail.com>
2022-06-28 12:55:28 +0000

    Mi_generador_de_excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#new-excuse").addEventListener("click", () => {
+    document.querySelector("#la-excusa").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomNumber = () => {
+  let selector1 = Math.random() * 5;
+  let selector2 = Math.floor(selector1);
+  return selector2;
+};
+
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Your teacher"];
+  let action = ["ate", "peed", "crushed", "broke", "burned"];
+  let what = ["my homework", "the keys", "the car", "my mom", "the house"];
+  let when = ["this morning", "the other day", "just then"];
+  let excuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)] +
+    ".";
+
+  return excuse;
 };
